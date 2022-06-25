13769ae9d8722b842602ce970aba20716de3097e
Bryan Garcia <bryan1997st@gmail.com>
2022-06-25 17:49:48 -0500

    :sparkles: Creates component file

---
+++ src/js/views/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Login = props => {
+    return (
+        <h1>Login 💪</h1>
+    )
+}
---
+++ src/js/views/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const NotFound = props => {
+    return (
+        <h1>Not found 🔨</h1>
+    )
+}
