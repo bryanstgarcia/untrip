ae4794acfe1f4454ba87a94c1f0834d916a1dc82
Bryan Garcia <bryan1997st@gmail.com>
2022-06-25 17:55:35 -0500

    :hammer: Adds theme handler

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,21 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			theme: "light",
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			handleTheme: () => {
+				const store = getStore()
+				if (store.theme == "light") {
+					setStore({ theme: "dark"})
+					return;
+				}
+				if (store.theme == "dark") {
+					setStore({ theme: "light"})
+					return;
+				}
+				alert("Existe un error con el tema")
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
