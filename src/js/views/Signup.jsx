db2c0bef59f7697710bbbe917fe3ce08316b4e5d
Bryan Garcia <bryan1997st@gmail.com>
2022-08-26 00:09:57 -0500

    :construction: Adds inputs and title

--- src/js/views/Signup.jsx
+++ src/js/views/Signup.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useContext, useState } from "react"
+import { InputField } from "../components/common/InputField.jsx"
+import { Context } from "../store/appContext"
 
 export const Signup = props => {
+    const { store, actions} = useContext(Context)
+    const [ name, setName ] = useState("")
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
     return (
-        <h1>Signup 🛩</h1>
+        <div className={`signup-box`}>
+            <h1 className={`signup__title ${store.theme}`}>
+                You are one step away <br /> from an awesome <br /> project management tool
+            </h1>
+            <div className={`signup___form`}>
+                <InputField 
+                    inputType="text"
+                    inputId="user-name"
+                    labelText="name"
+                    stateValue={name}
+                    setStateValue={setName}
+                />
+                <InputField 
+                    inputType="email"
+                    inputId="user-email"
+                    labelText="email"
+                    stateValue={email}
+                    setStateValue={setEmail}
+                />
+                <InputField 
+                    inputType="password"
+                    inputId="user-pass"
+                    labelText="password"
+                    stateValue={password}
+                    setStateValue={setPassword}
+                />
+            </div>
+        </div>
     )
 }
