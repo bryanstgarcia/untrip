fbe31f68203e22fbe22a5323f666dae574908e13
Bryan Garcia <bryan1997st@gmail.com>
2022-08-25 22:42:45 -0500

    :hammer: Creates and styles landing page component - :lipstick: Creates saas styles - :sparkles: Creates component structure

--- src/js/components/home/LandingPage.jsx
+++ src/js/components/home/LandingPage.jsx
@@ -1,16 +1,29 @@
 import React, { useContext } from "react"
 import { Context } from "../../store/appContext"
+import ArrowDown from "./../../../img/arrow-down.png"
 
 export const LandingPage = () => {
 	const { store, actions} = useContext(Context)
     
     return (
-        <div className={`landing ${store.theme} background`}>
-            <div className={`landing__bubbles-box first`}>
-                <div className={`bubbles-box__bubble ${store.theme}`}></div>
+        <div className={`landing ${store.theme} background`} id="landing-untrip">
+            <div className={`landing__blur`}>
+                <div className={`landing__bubbles-box first`}>
+                    <div className={`bubbles-box__bubble ${store.theme}`}></div>
+                </div>
+                <div className={`landing__bubbles-box second`}>
+                    <div className={`bubbles-box__bubble ${store.theme}`}></div>
+                </div>
             </div>
-            <div className={`landing__bubbles-box second`}>
-                <span className={`bubbles-box__bubble ${store.theme}`}></span>
+            <div className="landing__content">
+                <div className="landing__content--c2a">
+                    <h1>Handle your projects as pro as you want</h1>
+                    <button>Start project</button>
+                </div>
+                <a href="#about-untrip" className="landing__content--swipe">
+                    <p>swipe to the awesomeness</p>
+                    <img src={ArrowDown} alt="" />
+                </a>
             </div>
         </div>
     )
