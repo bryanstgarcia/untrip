d35500a3b4ec4fc891132ca7a15cf165759f05c3
Bryan Garcia <bryan1997st@gmail.com>
2022-08-25 22:49:07 -0500

    :hammer: Updates footer structure and styles - :lipstick: creates footer styles file and styled it - :hammer: Changes structure and information for Footer.jsx

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,10 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
+import { Context } from "../store/appContext";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+export const Footer = () => {
+	const { store, actions} = useContext(Context)
+	return (
+		<footer className={`footer-untrip ${store.theme} background-gray-scale`}>
+			<div className="footer-untrip__box">
+				<a className={`footer-untrip__box--title  ${store.theme} text-gray-scale`}>Untrip</a>
+				<ul className="footer-untrip__box--nav">
+					<li>
+						<a className={` ${store.theme} text-gray-scale`} href="">
+							Start Project
+						</a>
+					</li>
+					<li>
+						<a className={` ${store.theme} text-gray-scale`} href="">
+							About Untrip
+						</a>
+					</li>
+					<li>
+						<a className={` ${store.theme} text-gray-scale`} href="">
+							About us
+						</a>
+					</li>
+				</ul>
+			</div>
+		</footer>
+	);
+}
+
