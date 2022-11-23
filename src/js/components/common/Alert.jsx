0c00d332cafa8b4aece857bf68b8031a263d2a38
Bryan Garcia <bryan1997st@gmail.com>
2022-11-22 20:14:33 -0500

    :hammer: Creates Alert component - :art: Updates styles

---
+++ src/js/components/common/Alert.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { Context } from "../../store/appContext";
+
+const Alert = (props) => {
+    const { store } = useContext(Context)
+    return (
+        <aside className="alert-box">
+            <div className={`alert ${store.theme} background`}>
+                <div className={`alert__title ${store.theme} text`}>
+                    <h3 className={`${store.theme} text`}>{props.title}</h3>
+                    <span className={`${store.theme} text`}>x</span>
+                </div>
+                <div className="alert__body">
+                    <p className={`${store.theme} text`}>
+                        {props.body}
+                    </p>
+                    <p className={`${store.theme} text`}>
+                        {props.secundaryBody ? props.secundaryBody : ""}
+                    </p>
+                </div>
+
+            </div>
+        </aside>
+    )
+}
+
+export default Alert;
