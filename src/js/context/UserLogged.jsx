30402c7b9b637e7aa0fc420d6d22ad5d11c82513
Bryan Garcia <bryan1997st@gmail.com>
2022-11-22 20:20:14 -0500

    :hammer: Creates context to handle user Auth information

---
+++ src/js/context/UserLogged.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+export const UserLogged = React.createContext() 
+
+const UserLoggedProvider = ({ children }) => {
+    const [ user, setUser ] = useState({
+        info: null,
+        token: null,
+        refresh: null,
+        
+    })
+    return (
+        <UserLogged.Provider value={{ user, setUser }} >
+            { children }
+        </UserLogged.Provider>
+    )
+}
+
+export default UserLoggedProvider;
