cfce77f61a98a44d24dbcaeead840002e63cb79d
Bryan Garcia <bryan1997st@gmail.com>
2022-11-22 20:16:34 -0500

    :hammer: Updates file imports due to architecture changes

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ import "../styles/index.css";
 import './../styles/style.scss'
 
 //import your own components
-import Layout from "./layout";
+import Layout from "./Layout.jsx";
 
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
